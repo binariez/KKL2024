a7f17473491efe41d0efa4895ecd44e6d2d1a0e6
Azhar SS <azhacra@gmail.com>
2024-08-22 00:36:05 +0700

    Fixing..again..

--- api/Connection.php
+++ api/Connection.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/Connection.php';
 require_once __DIR__ . '/functions/Functions.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 $uri = 'mongodb+srv://' . $_ENV['MDB_USER'] . ':' . $_ENV['MDB_PASS'] . '@' . $_ENV['ATLAS_CLUSTER_SRV'] . '/?retryWrites=true&w=majority&appName=' . $_ENV['APP_NAME'];
 
