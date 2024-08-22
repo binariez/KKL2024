7e1eed15c415ef1d0fd39fff00cbf4834d8050b1
Azhar SS <azhacra@gmail.com>
2024-08-22 16:37:57 +0700

    added table structure for RUBBER

--- api/index.php
+++ api/index.php
@@ -82,8 +82,42 @@ require_once __DIR__ . '/template.php';
                 <tr style="border-color: black">
                     <td colspan="14" bgcolor="#d6dee0"><b>RUBBER</b></td>
                 </tr>
+
                 <?php
+                $i = 1;
+                foreach ($db->estate_sbe->find(["kategori" => "RUBBER"]) as $d) {
+                    foreach ($c = $db->estate_sbe_cek->find(["field" => $d["field"]]) as $cek) {
                 ?>
+                        <tr style="text-align: center; border-color: black">
+
+                            <!-- field & ha -->
+                            <td <?= $i % 2 == 1 ? 'bgcolor="8bd6f1"' : ''; ?>><?= $d['field'] ?></td>
+                            <td <?= $i % 2 == 1 ? 'bgcolor="8bd6f1"' : ''; ?>><?= $d['ha'] ?></td>
+
+                            <!-- ceklis pengecekan -->
+
+                            <!-- w1 -->
+                            <td><?= $cek['w1']['w1a'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w1']['w1b'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w1']['w1c'] != 'N/A' ? $cek['w1']['w1c'] : ''; ?></td>
+                            <!-- w2 -->
+                            <td><?= $cek['w2']['w2a'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w2']['w2b'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w2']['w2c'] != 'N/A' ? $cek['w2']['w2c'] : ''; ?></td>
+                            <!-- w3 -->
+                            <td><?= $cek['w3']['w3a'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w3']['w3b'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w3']['w3c'] != 'N/A' ? $cek['w3']['w3c'] : ''; ?></td>
+                            <!-- w4 -->
+                            <td><?= $cek['w4']['w4a'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w4']['w4b'] == 1 ? '&#x2714' : ''; ?></td>
+                            <td><?= $cek['w4']['w4c'] != 'N/A' ? $cek['w4']['w4c'] : ''; ?></td>
+
+                        </tr>
+
+                <?php }
+                    $i += 1;
+                } ?>
 
             </tbody>
         </table>
