ebc6644ec6b82e4be8f5b6d7afcbb0f5f568e596
Azhar SS <azhacra@gmail.com>
2024-08-29 14:57:19 +0700

    fixing routes

---
+++ api/GetFields.php
@@ -0,0 +1,76 @@
+<?php
+require_once __DIR__ . "/functions/Connection.php";
+require_once __DIR__ . "/functions/Sessions.php";
+
+$est = strtolower($_GET['estate']);
+$col = "estate_" . $est;
+
+$filter = ['kategori' => $_GET['kategori']];
+$options = ['sort' => ['_id' => 1]];
+$result = $db->$col->find($filter, $options);
+
+$count = $db->$col->aggregate([
+    [
+        '$facet' => [
+            'notFound' => [
+                [
+                    '$project' => [
+                        '_id' => 0,
+                        'count' => [
+                            '$const' => 0
+                        ]
+                    ]
+                ],
+                [
+                    '$limit' => 1
+                ]
+            ],
+            'found' => [
+                [
+                    '$match' => [
+                        'kategori' => $_GET['kategori']
+                    ]
+                ],
+                [
+                    '$count' => 'count'
+                ]
+            ]
+        ]
+    ],
+    [
+        '$replaceRoot' => [
+            'newRoot' => [
+                '$mergeObjects' => [
+                    [
+                        '$arrayElemAt' => [
+                            '$notFound',
+                            0
+                        ]
+                    ],
+                    [
+                        '$arrayElemAt' => [
+                            '$found',
+                            0
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+]);
+
+
+foreach ($count as $c) {
+    if ($c['count'] == 0) {
+
+?>
+        <option value="" hidden>-TIDAK ADA DATA-</option>
+        <?php
+    } else {
+        foreach ($result as $r) {
+        ?>
+            <option value="<?= $r['field'] ?>"><?= strtoupper($r['field']) ?></option>
+<?php
+        }
+    }
+}
